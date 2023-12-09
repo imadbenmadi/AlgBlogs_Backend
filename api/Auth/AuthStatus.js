863c655c5f7a438e9a1a9ca20f86ff53ad95ef32
imadbenmadi <imadbenmadi891@gmail.com>
2023-12-09 14:41:39 +0100

    Adding to the respons

--- api/Auth/AuthStatus.js
+++ api/Auth/AuthStatus.js
@@ -2,20 +2,35 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { Users } = require("../../models/Db");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     const accessToken = req.cookies.access_token;
     if (!accessToken) {
         return res.json({ isAuthenticated: false });
     }
+
     try {
-        const decoded = jwt.verify(
-            accessToken,
-            process.env.ACCESS_TOKEN_SECRET
-        );
-        return res.status(200).json({ isAuthenticated: true });
+        const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+        try {
+            const user = await Users.findOne(
+                { _id: decoded._id },
+                { UserName: 1, Profile_Pic: 1 }
+            );
+
+            if (!user) {
+                // User not found, handle accordingly (maybe consider logging out the user)
+                return res.status(401).json({ isAuthenticated: false });
+            }
+
+            return res.status(200).json({ isAuthenticated: true, user });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
     } catch (error) {
-        return res.status(200).json({ isAuthenticated: false });
+        return res.status(401).json({ isAuthenticated: false });
     }
 });
 
